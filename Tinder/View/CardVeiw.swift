86285a5a746010af85b34d2d710771c40213df75
yoshipjr <yoshihisa441396@gmail.com>
2021-03-03 18:23:32 +0900

    stackveviewの実装

--- Tinder/View/CardVeiw.swift
+++ Tinder/View/CardVeiw.swift
@@ -26,13 +26,26 @@ final class CardView: UIView {
         return label
     }()
     
+    let infoButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "comment_active"), for: .normal)
+//        button.imageView?.backgroundColor = .black
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = .green
+        
+        let baseStackView = UIStackView(arrangedSubviews: [infoButton])
+        baseStackView.axis = .horizontal
+        
         
         addSubview(cardImageView)
         addSubview(nameLabel)
+        addSubview(baseStackView)
+        
         cardImageView.anchor(top: topAnchor, bottom: bottomAnchor, left: leftAnchor, right: rightAnchor, leftPadding: 10, rightPadding: -10)
+        baseStackView.anchor(bottom: cardImageView.bottomAnchor, left: cardImageView.leftAnchor, right: cardImageView.rightAnchor, bottomPadding: 20, leftPadding: 20, rightPadding: 20)
         nameLabel.anchor(bottom: cardImageView.bottomAnchor, left: cardImageView.leftAnchor, bottomPadding: 40, leftPadding: 20)
     }
     
