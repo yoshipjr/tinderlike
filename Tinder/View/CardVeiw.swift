30aff7f130a1ae5ee5f238113c0967d52bd345c2
yoshipjr <yoshihisa441396@gmail.com>
2021-03-03 18:08:10 +0900

    cardviewにラベルの設定

--- Tinder/View/CardVeiw.swift
+++ Tinder/View/CardVeiw.swift
@@ -9,9 +9,31 @@ import Foundation
 import UIKit
 
 final class CardView: UIView {
+    
+    let cardImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .brown
+        iv.layer.cornerRadius = 10
+        iv.contentMode = .scaleToFill
+        return iv
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 40, weight: .heavy)
+        label.textColor = .white
+        label.text = "Taro 22"
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .green
+        
+        addSubview(cardImageView)
+        addSubview(nameLabel)
+        cardImageView.anchor(top: topAnchor, bottom: bottomAnchor, left: leftAnchor, right: rightAnchor, leftPadding: 10, rightPadding: -10)
+        nameLabel.anchor(bottom: cardImageView.bottomAnchor, left: cardImageView.leftAnchor, bottomPadding: 40, leftPadding: 20)
     }
     
     required init?(coder: NSCoder) {
