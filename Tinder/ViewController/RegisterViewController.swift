df7c4e536b64383cfcc81e4a67b0ec23c0f5aca4
yoshipjr <yoshihisa441396@gmail.com>
2021-03-07 14:41:58 +0900

    labelの追加

--- Tinder/ViewController/RegisterViewController.swift
+++ Tinder/ViewController/RegisterViewController.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 class RegisterViewController: UIViewController {
-    
+    private let titleLabel: UILabel = RegisterTitleLabel()
     private let nameTextField: UITextField = RegisterTextField.init(frame: .zero, type: .name)
     private let emailTextField: UITextField = RegisterTextField.init(frame: .zero, type: .email)
     private let passwordTextField: UITextField = RegisterTextField.init(frame: .zero, type: .password)
@@ -27,12 +27,16 @@ class RegisterViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .blue
         
-        let baseStackView = UIStackView(arrangedSubviews: [nameTextField, emailTextField, passwordTextField, registerButton])
+        let baseStackView = UIStackView(arrangedSubviews: [ nameTextField, emailTextField, passwordTextField, registerButton])
         baseStackView.axis = .vertical
         baseStackView.distribution = .fillEqually
         baseStackView.spacing = 20
+        
         view.addSubview(baseStackView)
+        view.addSubview(titleLabel)
+        
         nameTextField.anchor(height: 45)
         baseStackView.anchor(left: view.leftAnchor, right: view.rightAnchor, centerY: view.centerYAnchor, height: 200, leftPadding: 20, rightPadding: 20)
+        titleLabel.anchor(bottom: baseStackView.topAnchor, centerX: view.centerXAnchor, bottomPadding: 20)
     }
 }
