cd858a5853b281d024510c5386e8550a3426ca51
yoshipjr <yoshihisa441396@gmail.com>
2021-03-07 14:30:40 +0900

    registertextfiledをコンポーネント化

--- Tinder/ViewController/RegisterViewController.swift
+++ Tinder/ViewController/RegisterViewController.swift
@@ -10,19 +10,16 @@ import UIKit
 
 class RegisterViewController: UIViewController {
     
-    let nameTextField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "名前"
-        textField.borderStyle = .roundedRect
-        textField.font = .systemFont(ofSize: 14)
-        return textField
-    }()
+    private let nameTextField: UITextField = RegisterTextField.init(frame: .zero, type: .name)
+    private let emailTextField: UITextField = RegisterTextField.init(frame: .zero, type: .email)
+    private let passwordTextField: UITextField = RegisterTextField.init(frame: .zero, type: .password)
     
     let registerButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("登録", for: .normal)
         button.backgroundColor = .red
         button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 10
         return button
     }()
     
@@ -30,9 +27,12 @@ class RegisterViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .blue
         
-        let baseStackView = UIStackView(arrangedSubviews: [nameTextField, registerButton])
+        let baseStackView = UIStackView(arrangedSubviews: [nameTextField, emailTextField, passwordTextField, registerButton])
+        baseStackView.axis = .vertical
+        baseStackView.distribution = .fillEqually
+        baseStackView.spacing = 20
         view.addSubview(baseStackView)
-        
+        nameTextField.anchor(height: 45)
         baseStackView.anchor(left: view.leftAnchor, right: view.rightAnchor, centerY: view.centerYAnchor, height: 200, leftPadding: 20, rightPadding: 20)
     }
 }
