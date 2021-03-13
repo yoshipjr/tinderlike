e67db0ca51d85263388ce95e3617ac2ba28947dd
yoshipjr <yoshihisa441396@gmail.com>
2021-03-13 11:22:15 +0900

    uiview-extensionの作成

--- Tinder/Extension/UIvew+Extension.swift
+++ Tinder/Extension/UIvew+Extension.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+enum BorderPosition {
+    case top, left, right, bottom
+}
+
 extension UIView {
     // それぞれ、viewの制約をどこに合わせるかを設定する
     func anchor(top: NSLayoutYAxisAnchor? = nil,
@@ -54,6 +58,33 @@ extension UIView {
         
         if let height = height {
             self.heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    /// viewに枠線を表示する
+    /// - Parameters:
+    ///   - width: 太さ
+    ///   - color: 色
+    ///   - position: 場所
+    func addBorder(width: CGFloat, color: UIColor, position: BorderPosition) {
+        let border = CALayer()
+        
+        switch position {
+        case .top:
+            border.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: width)
+            border.backgroundColor = color.cgColor
+            self.layer.addSublayer(border)
+        case .left:
+            border.frame = CGRect(x: 0, y: 0, width: width, height: self.frame.height)
+            border.backgroundColor = color.cgColor
+            self.layer.addSublayer(border)
+        case .right:
+            border.frame = CGRect(x: self.frame.width - width, y: 0, width: width, height: self.frame.height)
+            border.backgroundColor = color.cgColor
+            self.layer.addSublayer(border)
+        case .bottom:
+            border.frame = CGRect(x: 0, y: self.frame.height - width, width: self.frame.width, height: width)
+            border.backgroundColor = color.cgColor
+            self.layer.addSublayer(border)
         }
     }
 }
